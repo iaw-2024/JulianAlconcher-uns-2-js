1489b63f029c07e03812e3961aa65c8a093811bd
Julian Alconcher <alconcherjulian@gmail.com>
2024-03-29 17:47:25 -0300

    Add index.ejs y getSong()

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
+app.set("view engine", "ejs");
+
+async function getSongs() {
+    const response = await fetch("https://6600c5a387c91a116419a29f.mockapi.io/songs");
+    const obj = await response.json();
+    return obj;
+}
+
+app.get("/express", (req, res) => {
+    getSongs().then((json) => res.render("index", { obj : json }));
+});
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-app.use(express.static('public'))
+
+app.use(express.static('public'));
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
