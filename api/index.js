9e74a1b9f95944a20f98a637c28015a28c8cd89d
Julian Alconcher <alconcherjulian@gmail.com>
2024-04-02 18:51:14 -0300

    Fix cliente_servidor funcionando

--- api/index.js
+++ api/index.js
@@ -17,24 +17,14 @@ app.get("/express", (req, res) => {
     getSongs().then((json) => res.render("index", { obj : json }));
 });
 
-// Ruta para obtener datos desde la API externa
-app.get('/cliente_servidor', async (req, res) => {
-  try {
+
+app.get('/cliente_servidor/get_datos', async (req, res) => {
       const response = await axios.get('https://6600c5a387c91a116419a29f.mockapi.io/songs');
-      console.log(response.data); // Imprimir datos recibidos del servidor externo
       const obj = response.data;
       res.json(obj);
-  } catch (error) {
-      console.error('Error al obtener datos de la API:', error);
-      console.error('Error de respuesta:', error.response.data); // Imprimir cualquier mensaje de error de la API externa
-      res.status(500).json({ error: 'Error al obtener datos de la API' });
-  }
 });
 
-
-
 app.use(express.static('public'));
-
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
--- public/cliente_servidor/script.js
+++ public/cliente_servidor/script.js
@@ -1,23 +1,20 @@
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/cliente_servidor")
+    fetch("/cliente_servidor/get_datos")
     .then(response => response.json())
     .then(data => {
         const ulSongs = document.getElementById('canciones-list');
-        data.forEach(data => {
-        const firstFiveSongs = data.slice(0, 5);
-            firstFiveSongs.forEach(element => {
-            let li = document.createElement("li");
-            li.textContent = `${element.song_name} - ${element.artist_name} `;
-            let p = document.createElement("p");
-            p.textContent = `${element.album_name}, Año: ${element.release_date.slice(0,4)}`;
-            li.appendChild(p);
-            ulSongs.appendChild(li);
+        const cinco = data.slice(0, 5);
+        cinco.forEach(element => {
+                let li = document.createElement("li");
+                li.textContent = `${element.song_name} - ${element.artist_name} `;
+                let h4 = document.createElement("h4");
+                h4.textContent = ` Album: ${element.album_name}, Año: ${element.release_date.slice(0,4)}`;
+                li.appendChild(h4);
+                ulSongs.appendChild(li);
         });
     })
     .catch(error => {
         console.error("Error al obtener el listado de clientes:", error);
     });
   });
-}) 
+  
